a72c925d76c30586ac9736381410f41cb0cbceb8
tijs.rademakers <tijs.rademakers@02d0656b-c235-0410-8c3a-05a010e55eb3>
2008-02-11 20:55:20 +0000

    Changed configuration to utilize latest Mule version

--- mule/src/esb/chapter4/component/transformer/StringToInteger.java
+++ mule/src/esb/chapter4/component/transformer/StringToInteger.java
@@ -6,12 +6,12 @@ import org.mule.transformer.AbstractTransformer;
 public class StringToInteger extends AbstractTransformer {
 
 	protected Object doTransform(Object payload, String encoding) throws TransformerException {
-		// We now the payload is String
+		// We know that the payload is String
 		String strPayload = (String) payload;
 		try {
 			return Integer.valueOf(strPayload);
 		} catch(Exception e) {
-			e.printStackTrace();
+			System.out.println("No valid Integer: " + strPayload);
 			return new Integer(-1);
 		}
 	}
