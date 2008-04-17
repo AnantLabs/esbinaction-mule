30d175f3fd3c78dd1af31a7feeed3e3e790b5f21
tijs.rademakers <tijs.rademakers@02d0656b-c235-0410-8c3a-05a010e55eb3>
2008-04-17 18:17:06 +0000

    Added JUnit test for scubadiving example

---
+++ trunk/mule/test/esb/chapter11/scubadiving/test/ScubaDivingTest.java
@@ -0,0 +1,36 @@
+package esb.chapter11.scubadiving.test;
+
+import java.util.GregorianCalendar;
+
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.junit.Test;
+
+import esb.chapter11.scubadiving.model.DivingInterest;
+import esb.chapter11.scubadiving.model.ScubaDivingRequest;
+
+public class ScubaDivingTest {
+	
+	@Test
+	public void goScubaDive() throws Exception {
+		ActiveMQConnection connection = ActiveMQConnection.makeConnection("tcp://localhost:61616");
+	    connection.start();
+	    Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createQueue("process.start");
+		ScubaDivingRequest request = new ScubaDivingRequest();
+		request.setGuestName("Mr Diver");
+		request.setInterest(DivingInterest.Sharks);
+		request.setLunchIncluded(true);
+		request.setNumberOfDivers(10);
+		request.setRoomNumber(600);
+		request.setRequestDate(new GregorianCalendar().getTime());
+		ObjectMessage objMessage = session.createObjectMessage();
+		objMessage.setObject(request);
+		MessageProducer producer = session.createProducer(destination);
+		producer.send(objMessage);
+	}
+}
