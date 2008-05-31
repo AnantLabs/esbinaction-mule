ed8886e74543b91054f1b0a3de2ff66193af25c4
andy.verberne <andy.verberne@02d0656b-c235-0410-8c3a-05a010e55eb3>
2008-05-31 09:34:25 +0000

    Updated to work with Mule 2.0.1

--- trunk/mule/src/esb/chapter4/messageflow/mule/BookQuoteAggregator.java
+++ trunk/mule/src/esb/chapter4/messageflow/mule/BookQuoteAggregator.java
@@ -8,7 +8,8 @@ import org.apache.log4j.Logger;
 import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleMessage;
-import org.mule.routing.AggregationException;
+import org.mule.api.routing.MessageInfoMapping;
+import org.mule.routing.EventCorrelatorCallback;
 import org.mule.routing.inbound.AbstractEventAggregator;
 import org.mule.routing.inbound.EventGroup;
 
@@ -18,39 +19,64 @@ public class BookQuoteAggregator extends AbstractEventAggregator {
 	
 	private static final Logger logger = Logger.getLogger(BookQuoteAggregator.class);
 	
-	protected Object getEventGroupIdForEvent(MuleEvent event) {
-		MuleMessage message = event.getMessage();
-		BookQuote quote = (BookQuote) message.getPayload();
-		return quote.getIsbn();
-	}
+	@Override
+	protected EventCorrelatorCallback getCorrelatorCallback() {
+		return new EventCorrelatorCallback()
+		{
 
-	protected boolean shouldAggregateEvents(EventGroup eventGroup) {
-		Iterator itEvent = eventGroup.iterator();
-		boolean isAmazonPresent = false;
-		boolean isBarnesPresent = false;
-		while(itEvent.hasNext()) {
-			MuleEvent event = (MuleEvent) itEvent.next();
-			BookQuote quote = (BookQuote) event.getMessage().getPayload();
-			String companyName = quote.getCompanyName();
-			logger.info("companyName present " + companyName);
-			if("Amazon".equalsIgnoreCase(companyName)) {
-				isAmazonPresent = true;
-			} else if("BarnesAndNoble".equalsIgnoreCase(companyName)) {
-				isBarnesPresent = true;
+			@Override
+			public MuleMessage aggregateEvents(EventGroup events) {
+				Iterator itEvent = events.iterator();
+				Collection<BookQuote> quoteList = new ArrayList<BookQuote>();
+				while(itEvent.hasNext()) {
+					MuleEvent event = (MuleEvent) itEvent.next();
+					BookQuote quote = (BookQuote) event.getMessage().getPayload();
+					quoteList.add(quote);
+				}
+				return new DefaultMuleMessage(quoteList);
 			}
-		}
-		return isAmazonPresent && isBarnesPresent;
+
+			@Override
+			public EventGroup createEventGroup(MuleEvent event, Object id) {
+				return new EventGroup(id, 2 );
+			}
+
+			@Override
+			public boolean shouldAggregateEvents(EventGroup events) {
+				Iterator itEvent = events.iterator();
+				boolean isAmazonPresent = false;
+				boolean isBarnesPresent = false;
+				while(itEvent.hasNext()) {
+					MuleEvent event = (MuleEvent) itEvent.next();
+					BookQuote quote = (BookQuote) event.getMessage().getPayload();
+					String companyName = quote.getCompanyName();
+					logger.info("companyName present " + companyName);
+					if("Amazon".equalsIgnoreCase(companyName)) {
+						isAmazonPresent = true;
+					} else if("BarnesAndNoble".equalsIgnoreCase(companyName)) {
+						isBarnesPresent = true;
+					}
+				}
+				return isAmazonPresent && isBarnesPresent;
+			}
+		};
 	}
 	
-	protected MuleMessage aggregateEvents(EventGroup eventGroup) throws AggregationException {
-		Iterator itEvent = eventGroup.iterator();
-		Collection<BookQuote> quoteList = new ArrayList<BookQuote>();
-		while(itEvent.hasNext()) {
-			MuleEvent event = (MuleEvent) itEvent.next();
-			BookQuote quote = (BookQuote) event.getMessage().getPayload();
-			quoteList.add(quote);
-		}
-		return new DefaultMuleMessage(quoteList);
-	}
+	@Override
+	public MessageInfoMapping getMessageInfoMapping() {
+		return new MessageInfoMapping()
+		{
+			@Override
+			public String getCorrelationId(MuleMessage message) {
+				BookQuote quote = (BookQuote) message.getPayload();
+				return quote.getIsbn();
+			}
 
+			@Override
+			public String getMessageId(MuleMessage message) {
+				BookQuote quote = (BookQuote) message.getPayload();
+				return quote.getIsbn();
+			}
+		};
+	}
 }
