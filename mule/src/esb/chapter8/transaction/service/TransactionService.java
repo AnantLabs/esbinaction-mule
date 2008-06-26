02a10a0fd0446e1539168433f6fba4d7a0ec5ab8
andy.verberne <andy.verberne@02d0656b-c235-0410-8c3a-05a010e55eb3>
2008-06-26 14:29:23 +0000

    Simplified version as explicitly rolling back the transaction is not needed.

--- mule/src/esb/chapter8/transaction/service/TransactionService.java
+++ mule/src/esb/chapter8/transaction/service/TransactionService.java
@@ -1,24 +1,14 @@
 package esb.chapter8.transaction.service;
 
-import org.mule.RequestContext;
-import org.mule.api.MuleEventContext;
-import org.mule.api.transaction.Transaction;
-
 public class TransactionService {
 	
 	public String processMessage(String message) throws Exception {
 		System.out.println("TransactionService received message " + message);
 		if(!"bad transaction".equalsIgnoreCase(message)) {
 			return "transaction succeeded";
+		} else {
+			throw new Exception("The transaction will fail");
 		}
-		MuleEventContext context = RequestContext.getEventContext();
-		Transaction transaction = context.getCurrentTransaction();
-		try {
-			transaction.setRollbackOnly();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		throw new Exception("The transaction has failed");
 	}
 
 }
