5cbaae2410ffd8e6f3469fa86a82e3af8dc90564
tijs.rademakers <tijs.rademakers@02d0656b-c235-0410-8c3a-05a010e55eb3>
2008-09-13 11:41:59 +0000

    updated start-up scripts Mule example chapter 4

--- mule/resources/chapter4/component/component.sh
+++ mule/resources/chapter4/component/component.sh
@@ -1,2 +1,2 @@
-export MULE_HOME=../../../../../../esb/mule-2.0.1
+export MULE_HOME=../../../../../../esb/mule-2.0.2
 $MULE_HOME/bin/mule -config "simplecomponent-config.xml"
--- mule/resources/chapter4/component/intcomponent.bat
+++ mule/resources/chapter4/component/intcomponent.bat
@@ -1,2 +1,2 @@
-SET MULE_HOME=..\..\..\..\..\..\esb\mule-2.0.1
+SET MULE_HOME=..\..\..\..\..\..\esb\mule-2.0.2
 %MULE_HOME%\bin\mule -config "simpleintcomponent-config.xml"
